739abac2a97414349e065e47932d5ec37c238b1c
mdobrovsky <mdobrovsky88@gmail.com>
2022-11-08 21:49:04 +0100

    ahoj :D

--- src/DrawingPanel.java
+++ src/DrawingPanel.java
@@ -15,6 +15,8 @@ public class DrawingPanel extends JPanel {
 		super.paint(g);
 		
 		Graphics2D g2 = (Graphics2D)g;
+		g2.scale(2.5,2.5);
+
 
 		//Ellipse2D s = new Ellipse2D.Double(100,100,20,20);
 		//Ellipse2D a = new Ellipse2D.Double(150,100,20,20);
@@ -28,11 +30,19 @@ public class DrawingPanel extends JPanel {
 		Path2D sa0_dolni = new Path2D.Double(new Line2D.Double(190,124,200,120));
 		//sa0.append(sa0_dolni);
 
+		Arc2D arc = new Arc2D.Double(120,85,100,45,20,140,Arc2D.OPEN);
+
+		QuadCurve2D q = new QuadCurve2D.Double(140,120,150,110,180,120);
+		g2.draw(q);
+		
+
+
 		g2.draw(s);
 		g2.draw(a);
 		g2.draw(sa0);
 		g2.draw(sa0_dolni);
 		g2.draw(sa0_horni);
+		//g2.draw(arc);
 
 
 
