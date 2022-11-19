1d06469f0ad703eb8440568e3ccb965b0bcf8f80
vitaliyb <vitalijbogera@gmail.com>
2022-11-19 14:05:50 +0100

    waka waka e e

--- src/DrawingPanel.java
+++ src/DrawingPanel.java
@@ -43,14 +43,15 @@ public class DrawingPanel extends JPanel {
 
         g2.drawLine(0,107,1200,107);
         g2.drawLine(0,535,1200,535);
-        g2.setFont(new Font("Times New", Font.TYPE1_FONT, 8));
+        g2.setFont(new Font("Times New", Font.TYPE1_FONT, 11));
         g2.drawString("*Pro ulehčení řešení situace, kdy se uživatel zmýlí v zadání " +
                 "vstupního symbolu nebo chce zkusit zadat jinou koncovou část řetězce, " +
-                "je implementována funkce Krok zpět. Konečný automat sám o sobě se k již zpracované " +
-                "části vstupního řetězce vracet nemůže.",20,545);
+                "je implementována funkce Krok zpět. Konečný automat sám o sobě se k již" ,20,545);
+        g2.drawString("zpracované " +
+                "části vstupního řetězce vracet nemůže.", 20, 560);
         g2.setFont(font);
-        g2.drawString("Vypracovali: Vitaliy Bohera, Martin Dobrovský", 20, 577);
-        g2.drawString("Fakulta aplikovaných věd Západočeské univerzity", 20, 594);
+        g2.drawString("Vypracovali: Vitaliy Bohera, Martin Dobrovský", 920, 577);
+        g2.drawString("Fakulta aplikovaných věd Západočeské univerzity", 920, 594);
         g2.scale(1.5, 1.5);
         g2.translate(-30, 95);
         g2.setStroke(new BasicStroke(2));
